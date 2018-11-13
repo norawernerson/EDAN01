9f1395884e92c6d61a16bf6c483b1866822c80d5
Nora Wernerson <dat14nwe@tstudent.lu.se>
2018-11-13 12:59:14 +0100

    First solution draft NOT WORKING

--- Lab1Photo/Main.java
+++ Lab1Photo/Main.java
@@ -26,20 +26,40 @@ public class Main{
     Constraint ctr = new Alldifferent(v);
     store.impose(ctr);
 
+    //atother domain
+    IntVar[] cost = new IntVar[prefs.length];
+    for (int i = 0; i < cost.length; i++){
+      cost[i] = new IntVar(store, 0, 1);
+    }
 
-    // define constraints
-    store.impose(new XeqY(v[0],  );
-    store.impose(new XneqY(v[0], v[2]) );
-    store.impose(new XneqY(v[1], v[2]) );
-    store.impose(new XneqY(v[1], v[3]) );
-    store.impose(new XneqY(v[2], v[3]) );
-    // search for a solution and print results
+    for(int i = 1; i < n_prefs-1 ; i++){
+      PrimitiveConstraint c1 = new XeqY(v[prefs[i][0]-1], new IntVar(store, v[prefs[i][1]-1].value()+1, v[prefs[i][1]-1].value()+1));
+      PrimitiveConstraint c2 = new XeqY(v[prefs[i][0]-1], new IntVar(store, v[prefs[i][1]-1].value()-1, v[prefs[i][1]-1].value()-1));
+      PrimitiveConstraint[] c3 = {c1, c2};
+      store.impose(new IfThenElse( new Or(c3), new XeqC(cost[i], 0), new XeqC(cost[i], 1)));
+      // if((v[prfes[i][0]].value() == v[prfes[i][1]]+1)) || (v[prfes[i][0]].value() == v[prfes[i][1]]-1))){
+      //   XeqC(cost[i], 0);
+      // }else{
+      //   XeqC(cost[i], 1);
+      // }
+    }
+    PrimitiveConstraint c1 = new XeqY(v[prefs[0][0]-1],new IntVar(store, v[prefs[0][1]-1].value()+1, v[prefs[0][1]-1].value()+1));
+    store.impose(new IfThenElse( c1, new XeqC(cost[0], 0), new XeqC(cost[0], 1)));
+    PrimitiveConstraint c2 = new XeqY(v[prefs[size-1][0]-1],new IntVar(store, v[prefs[size-1][1]-1].value()-1, v[prefs[size-1][1]-1].value()-1));
+    store.impose(new IfThenElse( c2, new XeqC(cost[size-1], 0), new XeqC(cost[size-1], 1)));
+
+    IntVar c = new IntVar(store, "cost", 0, n_prefs);
+    store.impose(new SumInt(store, cost, "==", c));
+
     Search<IntVar> search = new DepthFirstSearch<IntVar>();
     SelectChoicePoint<IntVar> select = new InputOrderSelect<IntVar>(store, v,new IndomainMin<IntVar>());
-    boolean result = search.labeling(store, select);
-    if( result )
+    boolean result = search.labeling(store, select, c);
+    if( result ){
+      System.out.println("number of prefs fulfilled: " + (n_prefs-c.value()));
       System.out.println("Solution: " + v[0]+", "+v[1] +", "+v[2] +", "+v[3]);
-    else
+    } else{
       System.out.println("***No");
+    }
+
   }
 }
