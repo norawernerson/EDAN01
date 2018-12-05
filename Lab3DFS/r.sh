e1953764b62701bdadd905b1153d7fac054901ff
Nora Wernerson <dat14nwe@tstudent.lu.se>
2018-12-05 09:33:44 +0100

    Fix r.sh file

--- Lab3DFS/r.sh
+++ Lab3DFS/r.sh
@@ -1,1 +1,1 @@
-javac -cp :/usr/local/cs/EDAN01/jacop.jar Main.java && java -cp :/usr/local/cs/EDAN01/jacop.jar Main
+javac -cp :/usr/local/cs/EDAN01/jacop.jar Golomb.java && java -cp :/usr/local/cs/EDAN01/jacop.jar Golomb
